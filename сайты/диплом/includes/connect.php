e9e3c94c8c3554378f176345f07bd6803bc9c816
Brandos251 <136456163+Brandos251@users.noreply.github.com>
2023-12-31 12:50:47 +0300

    Delete сайты/диплом directory

--- сайты/диплом/includes/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $connect = mysqli_connect('localhost', 'a0827644_users', 'H68dugUy', 'a0827644_users');
-
-    if(!$connect){
-        die("Ошибка подключения базы данных");
-    }
-    
-?>
-
